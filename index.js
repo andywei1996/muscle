632a4b11842f8101bf3a43912b4014417a61de6f
andywei <andywei@andysMacBook-Pro.local>
2017-07-06 15:38:21 +0800

    no message

---
+++ index.js
@@ -0,0 +1,24 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot =linebot({
+    channelId: 1523378933,  //註冊Line Bot的Channel ID
+    channelSecret: ae98c240a2aa06f654e873aefceb4e81,    //Channel Secret
+    channelAccessToken: jHRM9rD3O8NyJL8817rGwaWzimo15AUrWRj1oTnJLyjJC8L+guKxnO3KD1jfkBxXAvY75Xa1Pv/nW4GK0mvb9TpPp7Y53UPhuuf+KQdc9b2Vm60RwlrNFWwynvms+sOqQOZvfPGSAw1gOyLAKLwgBwdB04t89/1O/w1cDnyilFU=    //Channel Access Token
+});
+
+//印出從LINE收到的訊息
+bot.on('message',function(event){
+    console.log(event);
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//express預設是走port 3000，而heroku不是，所以以下程式碼將進行轉換
+var server = app.listen(process.env.PORT || 8000,function(){
+    var port = server.address().port;
+    console.log("App now running on port", port);
+
+});
