9abb051799b8989c9610f8fd8522256cc2c02fa4
andywei <andy1996andy@gmail.com>
2017-08-29 15:22:50 +0800

    no message

--- index.js
+++ index.js
@@ -69,7 +69,10 @@ bot.on('message',function(event){
         funccode = "home";funcstep = 0;
         event.reply("done.");
     }
-    else if (event.message.type == 'text' && event.message.text == "我想鍛鍊"){
+});
+
+bot.on('message', function(event){
+    if (event.message.type == 'text' && event.message.text == "我想鍛鍊"){
         var msg = {
             "type": "template",
             "altText": "您好，初次見面！",
@@ -98,7 +101,9 @@ bot.on('message',function(event){
             console.log('error');   //若有錯誤，catch下來後註記在log中
         });
     }
-    else if (event.message.type == 'text' && funccode == "home"){  //接收純文字內容
+});
+bot.on(function(event){
+    if (event.message.type == 'text' && funccode == "home"){  //接收純文字內容
         //var msg = event.message.text + " 收到！";
         
         //使用Jieba方法將接收到的文字內容(event.message.text)進行斷詞，並逐詞儲存至(_result)陣列
@@ -132,7 +137,74 @@ bot.on('message',function(event){
         });
         //=======================
     }
-    var BMRcoupute = require('./tdee.js');
+    else if(event.message.type = 'text' && funccode == "BMR"){
+        if (funcstep == 1){
+            if (event.message.text = "男") _sex = 5; //變數若未經var宣告即為全域變數
+            else _sex = -161;
+            event.reply("好的，請輸入您的年齡(實歲)");
+            funcstep = 2;
+        }
+        else if (funcstep == 2){
+            _age = Number(event.message.text);
+            event.reply("好的，接下來請輸入您的身高(公分)");
+            funcstep = 3;
+        }
+        else if (funcstep == 3){
+            _height = Number(event.message.text);
+            event.reply("最後請您請輸入您的體重(公斤)");
+            funcstep = 4;
+        }
+        else if (funcstep == 4){
+            _weight = Number(event.message.text);
+            bmr_result = (10 * _weight) + (6.25 * _height) - (5 * _age);// + _sex;
+            console.log(bmr_result);
+            var msg = {
+                "type": "template",
+                "altText": "BMR result replying template.",                
+                "template": {
+                    "type": "buttons",
+                    "text": "您的基礎代謝率是"+String(bmr_result)+"\n您想要知道您的TDEE嗎？",
+                    "actions":[
+                        {
+                            "type": "message",
+                            "label": "好R",
+                            "text": "好R"
+                        },
+                        {
+                            "type": "message",
+                            "label": "什麼是TDEE?",
+                            "text": "什麼是TDEE?"
+                        },
+                        {
+                            "type": "message",
+                            "label": "免了，謝謝",
+                            "text": "免了，謝謝"
+                        }
+                    ]
+                }
+            }
+            event.reply(msg).then(function(data){
+                console.log(msg);
+            }).catch(function(error){
+                console.log("ERROR"+error);
+            });
+                //"您的基礎代謝率是"+String(bmr_result)
+            //funccode = "home";
+            funcstep = 5;
+        }
+        else if (funcstep == 5){
+            if (event.message.text == "好R"){
+                event.reply("您的TDEE是"+(bmr_result - _sex)).then(function(data){
+                    console.log(bmr_result - _sex);
+                }); 
+                
+            }
+            
+            funccode = "home";
+            funcstep = 0;
+            
+        }
+    }
     
 });
 
