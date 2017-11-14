83e6e1c2ea7f9fb9366d02c01020cc74b4ad1ec0
andywei <andy1996andy@gmail.com>
2017-11-14 22:36:02 +0800

    no message

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ bot.on('message',function(event){
             type: 'text',
             text: '以下這個影片，將有效幫助你進行腿部的訓練，要注意觀看重點提示哦😉'
         };
-        event.pushMessage(id,msg);
+        bot.pushMessage(id,msg);
 
     }
 
